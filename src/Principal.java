bad6a47f27a7f4c917e0fea769753ceb6c11b510
Vergílio Valério dos Santos <vergilio.santos@hexagon.com>
2024-09-09 19:48:27 -0300

    get and sets

--- src/Principal.java
+++ src/Principal.java
@@ -3,17 +3,15 @@ import br.com.alura.screenmathc.modelos.Filme;
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Poderoso Chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("Poderoso Chefão");
+        meuFilme.setAnoDeLancamento(1980);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.setIncluidoNoPlano(true);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8  );
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-
-        System.out.println("Total das Avalições: " meuFilme.getSomaDasAvaliacoes());
-        System.out.println(meuFilme.totalAvaliacoes);
 
         System.out.println(meuFilme.pegamedia());
 
--- src/br/com/alura/screenmathc/modelos/Filme.java
+++ src/br/com/alura/screenmathc/modelos/Filme.java
@@ -8,13 +8,46 @@ public class Filme {
     private int totalAvaliacoes;
     private int duracaoEmMinutos;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
     public double getSomaDasAvaliacoes(){
         return  somaDasAvaliacoes;
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
     public void  exibeFichaTecnica (){
-        System.out.println("Nomde do filme: " +nome);
+        System.out.println("Nome de do filme: " +nome);
         System.out.println("Ano de Lançamento: "  +anoDeLancamento);
-        System.out.println("Duração do br.com.alura.screenmathc.modelos.Filme: " + duracaoEmMinutos);
+        System.out.println("Duração do Filme: " + duracaoEmMinutos);
 
     }
 
