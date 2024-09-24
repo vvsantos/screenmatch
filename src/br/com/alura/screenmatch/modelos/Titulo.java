1f8123b31cc15b7a154ed84883e8e84ce8541e11
Vergílio Valério dos Santos <vergilio.santos@hexagon.com>
2024-09-24 20:11:23 -0300

    Calculando diferença entre datas

--- src/br/com/alura/screenmatch/modelos/Titulo.java
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,11 +1,11 @@
-package br.com.alura.screenmathc.modelos;
+package br.com.alura.screenmatch.modelos;
 
-public class Filme {
+public class Titulo {
     private String nome;
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaDasAvaliacoes;
-    private int totalAvaliacoes;
+    private int totalDeAvaliacoes;
     private int duracaoEmMinutos;
 
     public String getNome() {
@@ -24,8 +24,8 @@ public class Filme {
         return duracaoEmMinutos;
     }
 
-    public double getSomaDasAvaliacoes(){
-        return  somaDasAvaliacoes;
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
     }
 
     public void setNome(String nome) {
@@ -44,20 +44,17 @@ public class Filme {
         this.duracaoEmMinutos = duracaoEmMinutos;
     }
 
-    public void  exibeFichaTecnica (){
-        System.out.println("Nome de do filme: " +nome);
-        System.out.println("Ano de Lançamento: "  +anoDeLancamento);
-        System.out.println("Duração do Filme: " + duracaoEmMinutos);
-
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
     }
 
     public void avalia(double nota){
         somaDasAvaliacoes += nota;
-        totalAvaliacoes++;
+        totalDeAvaliacoes++;
     }
 
-    public double pegamedia(){
-        return somaDasAvaliacoes / totalAvaliacoes;
+    public double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacoes;
     }
-
 }
