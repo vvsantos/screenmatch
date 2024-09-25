3e35dd4ca978e5372fdff0c51a75113d46674ce0
Vergílio Valério dos Santos <vergilio.santos@hexagon.com>
2024-09-25 08:59:02 -0300

    Calculando diferença entre datas

--- src/br/com/alura/screenmatch/Principal.java
+++ src/br/com/alura/screenmatch/Principal.java
@@ -6,6 +6,8 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
@@ -38,6 +40,20 @@ public class Principal {
         outroFilme.setAnoDeLancamento(2023);
         outroFilme.setDuracaoEmMinutos(200);
 
+        Filme filmeDoVergs = new Filme();
+        filmeDoVergs.setNome("Rei leão");
+        filmeDoVergs.setAnoDeLancamento(1995);
+        filmeDoVergs.setDuracaoEmMinutos(60);
+        filmeDoVergs.avalia(9);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(filmeDoVergs);
+        listaDeFilmes.add(outroFilme);
+        System.out.println("Tamanho da lista de Filmes " + listaDeFilmes.size());
+        System.out.println("Primeiro filme da lista é: " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
+
         CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
         calculadora.inclui(meuFilme);
         calculadora.inclui(outroFilme);
@@ -52,5 +68,7 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+
     }
 }
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -17,4 +17,9 @@ public class Filme extends Titulo implements Classificavel {
     public int getClassificacao() {
         return (int) pegaMedia() / 2;
     }
+
+    @Override
+    public String toString() {
+        return "O nome do Fime " + this.getNome() +"("+this.getAnoDeLancamento()+")";
+    }
 }
